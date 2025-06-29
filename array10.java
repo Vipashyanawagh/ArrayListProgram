4c19d5b778fa2270a0f847cf791b61638d4a3cf1
Vipashyanawagh <vipashyana.wagh2023@sait.ac.in>
2025-06-29 23:02:41 +0530

    array10 program

---
+++ array10.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Collections;
+public class array10 {
+   public static void main(String[] args) {
+// Create an ArrayList
+       ArrayList<String> colors = new ArrayList<>();
+// Add colors to the ArrayList
+       colors.add("Red");
+       colors.add("Green");
+       colors.add("Blue");
+       colors.add("Yellow");
+       colors.add("Orange");
+       System.out.println("Vipashyana Wagh 0873CS231135");
+       System.out.println("Original ArrayList: " + colors);
+// Sort the ArrayList
+       Collections.sort(colors);
+       System.out.println("Sorted ArrayList: " + colors);
+   }
+}
