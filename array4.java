fcf9c27b914a375869e500539e84aeb18fe28211
Vipashyanawagh <vipashyana.wagh2023@sait.ac.in>
2025-06-29 22:52:52 +0530

    array4 program

---
+++ array4.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+public class array4 {
+   public static void main(String[] args) {
+// Create an ArrayList
+       ArrayList<String> colors = new ArrayList<>();
+// Add colors to the ArrayList
+       colors.add("Red");
+       colors.add("Green");
+       colors.add("Blue");
+       colors.add("Yellow");
+       colors.add("Orange");
+// Print the ArrayList
+       System.out.print(" Vipashyana Wagh 0873CS231135");
+   }
+}
