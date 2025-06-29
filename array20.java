f43543bfded0320213b212b8dbaf9cfe666e7d4f
Vipashyanawagh <vipashyana.wagh2023@sait.ac.in>
2025-06-29 23:19:10 +0530

    array20 program

---
+++ array20.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+public class array20 {
+   public static void main(String[] args) {
+// Create an ArrayList
+       ArrayList<String> colors = new ArrayList<>();
+       colors.add("Red");
+       colors.add("Green");
+       colors.add("Blue");
+       System.out.println("Vipashyana Wagh  0873CS231135");
+       System.out.println("Original ArrayList: " + colors);
+// Empty the ArrayList
+       colors.clear();
+       System.out.println("ArrayList after clearing: " + colors);
+   }
+}
